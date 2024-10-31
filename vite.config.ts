bfb04703e62728b8f0e03ed692d62c1e8b4ce8ee
Moiri Gamboni <moiri.gamboni@proton.me>
2024-10-31 15:36:34 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vinted-calculator/",
+  base: "/VintedCalculator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
